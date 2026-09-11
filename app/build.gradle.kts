3078d50268034a55e4fe09fff897f2d09e6a706f
Boccia92 <rumepera1992@gmail.com>
2026-09-11 23:43:58 +0200

    Release v2.6 - Prevent Fire TV screen standby / screensaver during video playback

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.example.bocciatv"
         minSdk = 23
         targetSdk = 37
-        versionCode = 8
-        versionName = "2.5"
+        versionCode = 9
+        versionName = "2.6"
     }
 
     buildTypes {
