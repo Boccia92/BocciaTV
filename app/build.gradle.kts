e0f1c0df58aed5b8127d1bdd0ff05e4292c65680
Boccia92 <rumepera1992@gmail.com>
2026-09-11 16:10:49 +0200

    Release v2.2 - Clean app without integrated VPN

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.example.bocciatv"
         minSdk = 23
         targetSdk = 37
-        versionCode = 2
-        versionName = "2.0"
+        versionCode = 5
+        versionName = "2.2"
     }
 
     buildTypes {
