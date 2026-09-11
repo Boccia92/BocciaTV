16e5f08104767a4b64a5a3ccb6cfed958fb5b6ab
Boccia92 <rumepera1992@gmail.com>
2026-09-11 16:32:42 +0200

    Release v2.3 - New luxury golden logo background & icon

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.example.bocciatv"
         minSdk = 23
         targetSdk = 37
-        versionCode = 5
-        versionName = "2.2"
+        versionCode = 6
+        versionName = "2.3"
     }
 
     buildTypes {
