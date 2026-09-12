17aeb828b133e6e67a091f18ef3f7843ca8e7fec
Boccia92 <boccia92@users.noreply.github.com>
2026-09-12 16:40:17 +0200

    Force trigger update v6.0 (versionCode 200)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.example.bocciatv"
         minSdk = 23
         targetSdk = 37
-        versionCode = 120
-        versionName = "5.0"
+        versionCode = 200
+        versionName = "6.0"
     }
 
     buildTypes {
