8509948fbd96aebc1cc51f8f90b466f723c88734
Boccia92 <boccia92@users.noreply.github.com>
2026-09-21 14:03:49 +0200

    Bump version to 428 (v18.7) release update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.example.bocciatv"
         minSdk = 23
         targetSdk = 37
-        versionCode = 427
-        versionName = "18.6"
+        versionCode = 428
+        versionName = "18.7"
     }
 
     buildTypes {
